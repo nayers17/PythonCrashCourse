c642aea0a636b8d2836d6b586c4b220ebcc37254
nayers17 <nathanayers17@gmail.com>
2025-06-15 19:20:49 -0400

    added python crash course to repo

--- chapter2/hello_world.py
+++ chapter2/hello_world.py
@@ -43,3 +43,4 @@ print(message)
 # message = 'One of Python's strengths is its diverse community.'
 
 # print(message)
+
--- chapter2/name_cases.py
+++ chapter2/name_cases.py
@@ -1,5 +1,10 @@
+# Here I am defining a variable named "name" and assigning it the value of "Evan Slaski"
+
 name = "Evan Slaski"
 
+# below, I am using the print function to return the value of "name" value 
+# and using methods such as lower(), upper(), and title() to change the casing
+# on the print function that directly affects the capitalization on the string value
 print(name.lower())
 print(name.upper())
 print(name.title())
