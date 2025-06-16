f6661f0cbd03774d832a3e36a3d45fa04fdad1e0
nayers17 <nathanayers17@gmail.com>
2025-06-15 20:30:43 -0400

    completed 3.4-3.7

---
+++ chapter3/3_4guestlist.py
@@ -0,0 +1,57 @@
+invites = ['evan', 'tristan', 'meow']
+
+print(f"Hi {invites[0].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"Hi {invites[1].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"Hi {invites[2].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+
+friendwhocantgo = invites.pop(0)
+
+print(friendwhocantgo.title())
+print(f"\nMy dear friend {friendwhocantgo.title()} is unable to attend because he has a preexisting arrangement scheduled with his significant other.")
+
+
+
+invites.insert(0, 'trident')
+print(invites)
+
+print(f"\nHi {invites[0].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"Hi {invites[1].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"Hi {invites[2].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+
+print(f"\n Hey, {invites[0].title()}, {invites[1].title()}, {invites[2].title()}, I found a bigger table!")
+
+invites.insert(0, 'kai')
+invites.insert(2, 'Jausho')
+invites.append('Dark')
+
+print(invites)
+
+print(f"\nHi {invites[0].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"\nHi {invites[1].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"\nHi {invites[2].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"\nHi {invites[3].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"\nHi {invites[4].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+print(f"\nHi {invites[5].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
+
+print(f"Hey, {invites[0].title()}, {invites[1].title()}, {invites[2].title()}, {invites[3].title()}, {invites[4].title()}, {invites[5].title()}- I can only invite two people for dinner.")
+
+Dark = invites.pop()
+Meow = invites.pop()
+Tristan = invites.pop()
+Jausho = invites.pop()
+
+print(invites)
+
+print(f"\nhey {Dark.title()}, I'm sorry but our table shrunk and we don't have room for you.")
+print(f"\nhey {Meow.title()}, I'm sorry but our table shrunk and we don't have room for you.")
+print(f"\nhey {Tristan.title()}, I'm sorry but our table shrunk and we don't have room for you.")
+print(f"\nhey {Jausho.title()}, I'm sorry but our table shrunk and we don't have room for you.")
+print(f"\nHey {invites[0].title()}, Our table shrunk by 4, but you're still invited!")
+print(f"\nHey {invites[1].title()}, Our table shrunk by 4, but you're still invited!")
+
+del invites[0]
+del invites[0]
+print(invites)
+
+complete = '3.4-3.7: Complete!'
+print(complete)
--- chapter3/listpractice.py
+++ chapter3/listpractice.py
@@ -10,6 +10,7 @@
 
 # print(message)
 
+## creating a list of motorcycles
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
@@ -17,11 +18,16 @@ print(motorcycles)
 # print(motorcycles[0])
 # print(motorcycles)
 
+
+## adding a ducati to the end of the list with append method
 motorcycles.append('ducati')
 print(motorcycles)
 
+
+## reassigning an empty list value to motorcycles to practice appending to a list
 motorcycles = []
 print(motorcycles)
+
 
 motorcycles.append('honda')
 motorcycles.append('yamaha')
@@ -29,11 +35,50 @@ motorcycles.append('suzuki')
 
 print(motorcycles)
 
+
+## practicing with insert vs. append
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
 
 motorcycles.insert(2, 'example')
 print(motorcycles)
 
+## practicing the delete function, deleting the element at index 2
 del motorcycles[2]
 print(motorcycles)
+
+## practicing pop: taking the last element from the last, removing it from the list,
+# and assigning the variable "popped_motorcycle" the value of the removed element "suzuki"
+popped_motorcycle = motorcycles.pop()
+print(popped_motorcycle)
+
+# checking to see if it actually removed it from the list assigned to motorcycles (yes, it did)
+print(motorcycles)
+
+## real world example of using pop
+print(f"The last motorcycle I bought was a {popped_motorcycle.title()}.")
+
+first_motorcycle_purchase = motorcycles.pop(0)
+print(first_motorcycle_purchase)
+
+print(f"The first motorcycle I ever purchased was a {first_motorcycle_purchase.title()}.")
+
+motorcycles.append('suzuki')
+motorcycles.append('ducati')
+print(motorcycles)
+
+# practicing with .remove() method: if you don't know the index of the element, but you
+# know the name of the element, then you should use the .remove() method 
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+## removing an element with a variable that holds a value of the element we want to remove
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nThe {too_expensive.title()} is too expensive for me to insure and maintain, so I sold it.")
+
