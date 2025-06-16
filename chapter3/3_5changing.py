13ece8c58f2c2ea95a3727c1b6c2e16c78cfb84f
nayers17 <nathanayers17@gmail.com>
2025-06-15 20:31:03 -0400

    removed unneeded file

--- chapter3/3_5changing.py
+++
@@ -1,7 +0,0 @@
-invites = ['evan', 'tristan', 'meow']
-
-print(f"Hi {invites[0].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
-print(f"Hi {invites[1].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!\n")
-print(f"Hi {invites[2].title()}, \nI'm inviting you out to dinner with me tonight. I hope you can make it!")
-
-print(invites[0])
