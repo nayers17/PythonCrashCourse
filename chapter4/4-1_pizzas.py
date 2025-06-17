20c1154b6b376ee6bcf8081365cab2db63e8d564
Nathan <naprimarycontact@gmail.com>
2025-06-17 16:15:11 -0400

    4-2

--- chapter4/4-1_pizzas.py
+++ chapter4/4-1_pizzas.py
@@ -1,4 +1,11 @@
 pizzas = ['stoners', 'papa johns', 'little caesars', 'dominos']
 
 for pizza in pizzas:
-    print(pizza)
+    print(f"this is the name of the pizza company: {pizza}")
+
+print(f"\n{pizzas[0]} has a great quality to price ratio.\n{pizzas[1]} also is priced similarly, but not quite as delicious.\n{pizzas[2]} is definitely the lowest of quality and cheapest, nonetheless I still enjoy it immensely due to my love of pizza!\nI really love pizza!")
+
+animals = [' dog', ' cat', 'n eagle']
+
+for animal in animals:
+    print(f'A{animal} would make a great pet!')
