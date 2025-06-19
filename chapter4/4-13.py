80b0e061adda22089c6cd871e2ae2441089bb3cb
Nathan <naprimarycontact@gmail.com>
2025-06-19 09:00:29 -0400

    4-13 finished: tuples

---
+++ chapter4/4-13.py
@@ -0,0 +1,10 @@
+foods = ('chicken', 'steak', 'rice', 'ham', 'pasta')
+
+for food in foods:
+    print(food)
+
+# foods[0] = 'salad'
+
+foods = ['chicken', 'steak', 'lasagna', 'bologna', 'ramen']
+for food in foods:
+    print(food)
---
+++ chapter4/dimensions.py
@@ -0,0 +1,13 @@
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+# my_t = (3,)
+# print(my_t[0])
+
+
+dimensions= (400,100)
+print("\nModified dimensions:")
+for dimensions in dimensions:
+    print(dimension)
